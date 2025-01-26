f3817b53043a9d58a6cc588dfba8fc21cc7b8bd6
exg1o <borodenko22448811@gmail.com>
2025-01-26 18:32:18 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -7,19 +7,22 @@ pip install poetry
 poetry install
 spotdl --download-ffmpeg
 
-echo ""
-echo "Helper links:"
-echo "1. https://core.telegram.org/bots/features#creating-a-new-bot"
-echo "2. https://developer.spotify.com/documentation/web-api/concepts/apps"
-echo ""
-
 read -p "Enter Telegram bot API-Token: " API_TOKEN
 read -p "Enter Spotify client id: " SPOTIFY_CLIENT_ID
 read -p "Enter Spotify client secret: " SPOTIFY_CLIENT_SECRET
+read -p "Enter PostgreSQL database host: " POSTGRESQL_DATABASE_HOST
+read -p "Enter PostgreSQL database name: " POSTGRESQL_DATABASE_NAME
+read -p "Enter PostgreSQL database user: " POSTGRESQL_DATABASE_USER
+read -p "Enter PostgreSQL database password: " POSTGRESQL_DATABASE_PASSWORD
 
 cat << EOF > .env
 BOT_TOKEN=$BOT_TOKEN
 
 SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID
 SPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET
+
+POSTGRESQL_DATABASE_HOST=$POSTGRESQL_DATABASE_HOST
+POSTGRESQL_DATABASE_NAME=$POSTGRESQL_DATABASE_NAME
+POSTGRESQL_DATABASE_USER=$POSTGRESQL_DATABASE_USER
+POSTGRESQL_DATABASE_PASSWORD=$POSTGRESQL_DATABASE_PASSWORD
 EOF
