b40434e71a5966370f85c5ec3979718f4fbcb63c
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-05-07 16:58:21 +0300

    Refactor `bot` package (#26)

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Chat, Message, User
+from aiogram.types import Message, User
 
 from core.settings import BOT_TOKEN
 from spotify import Song, spotify
@@ -30,25 +30,25 @@ dispatcher.update.middleware(CreateUserMiddleware())
 async def start_command_handler(message: Message, event_from_user: User) -> None:
     await message.answer(
         (
-            f'Hello, {event_from_user.full_name}!\n\n'
-            'I\'m a free <a href="https://github.com/EXG1O/Spotify-Music-Downloader-Telegram-Bot">open-source</a> '
-            'Telegram bot for downloading music from Spotify using your link.\n\n'
-            'Send me a Spotify link, and I will download its content and send it to you.\n\n'
-            'If you like how I work and want to support the developer, '
-            'you can make a donation using one of the methods mentioned in this <a href="https://t.me/exg1o_channel/107">post</a>.\n\n'
-            "You can also subscribe to the @exg1o_channel, the developer's Telegram channel, to stay updated on news and other projects.\n\n"
-            '<b>Note:</b> downloading music from a Spotify playlist may not always work correctly.'
+            f'<b>Hi, {event_from_user.full_name}!</b>\n\n'
+            'I\'m a free <a href="https://github.com/EXG1O/Spotify-Music-Downloader-Telegram-Bot">open-source</a> Telegram bot '
+            'that helps you download music from Spotify — just send me a link.\n\n'
+            'If you want to support the developer, you can make a donation using one of '
+            'the methods mentioned in this <a href="https://t.me/exg1o_channel/107">post</a>.\n\n'
+            "You can also subscribe to the @exg1o_channel, the developer's Telegram channel, "
+            'to stay updated on news and other projects.\n\n'
+            '<b>Note:</b> playlist downloads may not always work perfectly.'
         ),
     )
 
 
 @dispatcher.message()
-async def message_handler(message: Message, event_chat: Chat) -> None:
+async def message_handler(message: Message) -> None:
     if not message.text:
         return None
 
     bot_message: Message = await message.reply(
-        'Downloading music from the link...\nThis process may take a few minutes.'
+        'Downloading music... This might may take a few minutes.'
     )
 
     try:
@@ -58,7 +58,7 @@ async def message_handler(message: Message, event_chat: Chat) -> None:
 
     if not songs:
         await bot_message.edit_text(
-            'Could not find or download music at the link, please try again later or send a different link.'
+            "I couldn't download anything from this link. Please try another one"
         )
         return
 
