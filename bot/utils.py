eef4f88ffe835365ef7009538ebc3cf5cf033e8e
exg1o <exg1o.me@gmail.com>
2025-05-11 06:51:58 +0300

    Create music download queue

--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,25 @@
-from aiogram.types import FSInputFile, Message
+from aiogram.types import FSInputFile
 
 from spotify import Song
 
+from . import bot
+
 from pathlib import Path
+from typing import Any
 
 
-async def reply_song(message: Message, song: Song, path: Path | None) -> None:
-    if path:
-        await message.reply_audio(FSInputFile(path))
+async def reply_song(
+    chat_id: int, user_message_id: int, song: Song, song_path: Path | None
+) -> None:
+    bot_message_kwargs: dict[str, Any] = {
+        'chat_id': chat_id,
+        'reply_to_message_id': user_message_id,
+    }
+
+    if song_path:
+        await bot.send_audio(**bot_message_kwargs, audio=FSInputFile(song_path))
     else:
-        await message.reply(f'Failed to download <code>{song.display_name}</code>.')
+        await bot.send_message(
+            **bot_message_kwargs,
+            text=f'Failed to download <code>{song.display_name}</code>.',
+        )
