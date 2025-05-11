3457cf965466dca07c90c70ec67878d1d3741e8a
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-05-11 04:49:44 +0300

    Add and configure `Alembic` for future db migrations (#29)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from bot import bot, dispatcher
 from core.tasks import run_tasks
-from database import create_database_tables
 
 import asyncio
 
 
 async def main() -> None:
-    await create_database_tables()
     await run_tasks()
     await dispatcher.start_polling(bot)
 
