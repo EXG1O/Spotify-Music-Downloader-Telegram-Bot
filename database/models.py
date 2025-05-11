42db38a340964faaa2664fd753be92cdb0d25590
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-05-11 05:23:01 +0300

    Add `joined_date` field to `User` model (#30)

--- database/models.py
+++ database/models.py
@@ -1,6 +1,9 @@
+from sqlalchemy import TIMESTAMP, text
 from sqlalchemy.dialects.postgresql import BIGINT
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from datetime import datetime
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -12,3 +15,6 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(BIGINT(), primary_key=True)
     telegram_id: Mapped[int] = mapped_column(BIGINT(), unique=True, index=True)
+    joined_date: Mapped[datetime] = mapped_column(
+        TIMESTAMP(), server_default=text('NOW()')
+    )
